ee5810b2f16acbf9666d469433649a537e2b320c
zub <crazysal.14@gmail.com>
2017-10-26 21:03:30 -0400

    outputs generated

--- test/show_all_circles.m
+++ test/show_all_circles.m
@@ -25,5 +25,5 @@ theta = theta(ones(size(cx1,1),1),:);
 X = cx1+cos(theta).*rad1;
 Y = cy1+sin(theta).*rad1;
 line(X', Y', 'Color', color, 'LineWidth', ln_wid);
-
+sprintf('%d circles', size(cx,1))
 title(sprintf('%d circles', size(cx,1)));
